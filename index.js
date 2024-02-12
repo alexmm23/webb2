2a8c2c0f8745aba49bc5e89deef15050ceda267f
alexmm23 <alejandromontes393@gmail.com>
2024-02-12 13:31:17 -0600

    Endpoint consulta individual

--- index.js
+++ index.js
@@ -21,14 +21,16 @@ app.listen(3000, () => console.log('Server is running on port 3000'))
 app.get('/', (req, res)=>{
     res.send('hello world!')
 })
-
+app.get('/user/:id',(req, res)=>{
+    let user=users.find((item) => item.id === parseInt(req.params.id));
+        res.send({msg:"Tu usuario", user});
+})
 app.get('/users', (req, res)=>{
     res.send(users)
 })
 
 app.post('/create/user/', (req, res)=>{
     const {nombre,id}= req.body
-    console.log(req.body)
     users.push({nombre,id})
     console.log('create user')
     res.send({msg:"User created", users})
